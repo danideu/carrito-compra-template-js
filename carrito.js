9ca4538ea64a72345e86eb728afd5be078787aff
danideu <danideu@gmail.com>
2023-02-06 00:35:35 +0100

    Carrito compra Javascript

---
+++ carrito.js
@@ -0,0 +1,41 @@
+const carrito = document.querySelector('#carrito')
+const template = document.querySelector('#template')
+const fragment = document.createDocumentFragment()
+
+const botones = document.querySelectorAll('.card .btn')
+
+const carritoObjeto = {}
+
+const agregarAlCarrito = (e) => {
+    //El dataset nos hace referencia al data del button, en este caso data-fruit
+    console.log(e.target.dataset.fruta)
+    const producto = {
+        titulo: e.target.dataset.fruta,
+        id: e.target.dataset.fruta,
+        cantidad: 1
+    }
+
+    if( carritoObjeto.hasOwnProperty(producto.titulo) ){
+        producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1
+    }
+    carritoObjeto[producto.titulo] = producto
+    pintarCarrito()
+    console.log(carritoObjeto)
+}
+
+const pintarCarrito = () => {
+    
+    carrito.textContent = ""
+
+    Object.values(carritoObjeto).forEach(item => {
+        const clone = template.content.firstElementChild.cloneNode(true)
+        clone.querySelector('.lead').textContent = item.titulo
+        clone.querySelector('.badge').textContent = item.cantidad
+        fragment.appendChild(clone)
+    })
+
+    carrito.appendChild(fragment)
+    
+}
+
+botones.forEach(btn => btn.addEventListener("click", agregarAlCarrito))
